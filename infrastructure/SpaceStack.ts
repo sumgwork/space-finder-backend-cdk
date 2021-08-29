095895cc178d040e694980d3d4f2a04a882683cb
Sumit Govil <sumg.work@gmail.com>
2021-08-29 22:30:54 +1000

    List s3 buckets, policy, and debug launch config

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -1,14 +1,10 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
-import {
-  Code,
-  Function as LambdaFunction,
-  Runtime,
-} from "aws-cdk-lib/lib/aws-lambda";
 import { LambdaIntegration, RestApi } from "aws-cdk-lib/lib/aws-apigateway";
 import { join } from "path";
 import { GenericTable } from "./GenericTable";
 import { NodejsFunction } from "aws-cdk-lib/lib/aws-lambda-nodejs";
+import { PolicyStatement } from "aws-cdk-lib/lib/aws-iam";
 
 export class SpaceStack extends Stack {
   private api = new RestApi(this, "spaceApi");
@@ -17,19 +13,22 @@ export class SpaceStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
 
-    const helloLambda = new LambdaFunction(this, "helloLambda", {
-      runtime: Runtime.NODEJS_14_X,
-      code: Code.fromAsset(join(__dirname, "..", "services", "hello")),
-      handler: "hello.main",
-    });
-
+    // Lambda function
     const helloLambdaNodejs = new NodejsFunction(this, "helloLambdaNodejs", {
       entry: join(__dirname, "..", "services", "node-lambda", "hello.ts"),
       handler: "handler",
     });
 
+    // New policy for listing s3 buckets
+    const s3ListPolicy = new PolicyStatement();
+    s3ListPolicy.addActions("s3:ListAllMyBuckets");
+    s3ListPolicy.addResources("*");
+
+    // attach this policy to lambda function
+    helloLambdaNodejs.addToRolePolicy(s3ListPolicy);
+
     // Hello api lambda integration
-    const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+    const helloLambdaIntegration = new LambdaIntegration(helloLambdaNodejs);
     const helloLambdaResource = this.api.root.addResource("hello");
     helloLambdaResource.addMethod("GET", helloLambdaIntegration);
   }
--- services/node-lambda/hello.ts
+++ services/node-lambda/hello.ts
@@ -1,12 +1,17 @@
 import { v4 } from "uuid";
+import { S3 } from "aws-sdk";
+
+const s3Client = new S3();
 
 async function handler(event: any, context: any) {
+  const buckets = await s3Client.listBuckets().promise();
   return {
     statusCode: 200,
-    body: {
+    body: JSON.stringify({
       message: "Hello from TS lambda",
       id: v4(),
-    },
+      buckets: buckets.Buckets,
+    }),
   };
 }
 
